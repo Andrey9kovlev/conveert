040a51a52c037907037dead152ea46ab031c7326
Andrey <andrey9kovlev@mail.ru>
2023-01-16 16:10:17 +0300

    3commit

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ function convert (amount, curr) {
 }
 convert(1000, usdCurr);
 convert (500, eurCurr);
+
+
+let num = 20;
+
+function showFirstMessage(text) {
+    console.log(text);
+    console.log(num);
+}
+showFirstMessage("erer");
+console.log(num);
+
+
+function ret() {
+    let num = 50;
+    return num;
+}
+console.log(ret());
+
+const logger = function () {
+console.log("dkg");
+};
+
+logger ();
+
+const calc = (a, b) => {return a+b};
+console.log(calc(5,6));
